9ba8a2f85dd9268cd11d0a0eea21eef614b5079c
York <kmgs4524@gmail.com>
2017-08-01 22:55:40 +0800

    Create deleteData.js

--- entry.js
+++ entry.js
@@ -1,14 +1,23 @@
 import $ from 'jquery';
 import getForm from "./lib/form.data.js";
 import insertData from "./lib/insertData.js";
+import deleteData from "./lib/deleteData.js";
 
 let form = $("form");
+let todo = $("ul.todo");
 
-form.on('submit', function (e) {
+form.on('submit', function (e) {	
 	e.preventDefault();
 	var value = getForm(e);
 	insertData(value);
 	alert(`Add item: ${value}`);
+	console.log("submit");
+});
+
+todo.on('click', ".btnDel",function(e) {	//e: EventObject
+	//$(this).parent().remove();
+	deleteData($(this));
+	console.log($(this));
 });
 
 
