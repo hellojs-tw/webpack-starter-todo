a1ccc4c38871d84beb6d4e0484af25a93cb62736
yubin551 <marks1994abc@gmail.com>
2017-08-06 18:59:18 +0800

    fix

--- entry.js
+++ entry.js
@@ -1,15 +1,19 @@
 import $ from 'jquery';
 import getForm from "./lib/form.data.js";
 import insertData from "./lib/insertData.js";
+import deleteData from "./lib/deleteData.js";
 
 let form = $("form");
 
-form.on('submit', function (e) {
-	e.preventDefault();
-	var value = getForm(e);
-	insertData(value);
-	alert(`Add item: ${value}`);
+form.on('submit', function(e) {
+    e.preventDefault();
+    var value = getForm(e);
+    insertData(value);
+    alert(`Add item: ${value}`);
 });
 
-
-
+$('ul.todo').on('click', '.delete', (e) => {
+    let text = e.currentTarget.parentElement.firstChild.textContent;
+    if (confirm(`確定刪除:${text}？`))
+        deleteData(e.currentTarget.parentElement);
+});
