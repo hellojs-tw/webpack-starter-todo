f13de8d831372eaed9a954fad605a2c67406e661
yubin551 <marks1994abc@gmail.com>
2017-08-10 02:58:24 +0800

    modify listen event

--- entry.js
+++ entry.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 import getForm from "./lib/form.data.js";
 import insertData from "./lib/insertData.js";
-import deleteData from "./lib/deleteData.js";
 
 let form = $("form");
 
@@ -10,10 +9,4 @@ form.on('submit', function(e) {
     var value = getForm(e);
     insertData(value);
     alert(`Add item: ${value}`);
-});
-
-$('ul.todo').on('click', '.delete', (e) => {
-    let text = e.currentTarget.parentElement.firstChild.textContent;
-    if (confirm(`確定刪除:${text}？`))
-        deleteData(e.currentTarget.parentElement);
 });
